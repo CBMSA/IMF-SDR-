1dfc88fd7c845171864de5edcefb29ebebdf6513
CBMSA <153248452+CBMSA@users.noreply.github.com>
2025-05-27 11:39:21 +0200

    IMFSDRIssuer.sol

---
+++ IMFSDRIssuer.sol
@@ -0,0 +1,20 @@
+Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SDRIssuer {
+    address public admin;
+    mapping(address => uint256) public sdrBalances;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function assignSDR(address recipient, uint256 amount) external {
+        require(msg.sender == admin, "Only admin can assign SDR");
+        sdrBalances[recipient] += amount;
+    }
+
+    function getSDRBalance(address recipient) external view returns (uint256) {
+        return sdrBalances[recipient];
+    }
+}
