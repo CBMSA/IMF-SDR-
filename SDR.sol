2f891ddee34f22e7440fb47c789ba6ca1b9e2aca
CBMSA <153248452+CBMSA@users.noreply.github.com>
2025-05-27 19:20:37 +0200

    SDR.sol

--- SDR.sol
+++ SDR.sol
@@ -1,4 +1,4 @@
-
+ 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -10,18 +10,62 @@ contract SDRIssuer {
 
     address public admin;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Issued(address indexed to, uint256 amount);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 
     constructor() {
         admin = msg.sender;
         totalSupply = 0;
     }
 
-    function issueSDR(address to, uint256 amount) external {
-        require(msg.sender == admin, "Only admin can issue SDR");
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can perform this action");
+        _;
+    }
+
+    function issueSDR(address to, uint256 amount) external onlyAdmin {
+        require(to != address(0), "Invalid recipient");
         balanceOf[to] += amount;
         totalSupply += amount;
         emit Issued(to, amount);
+        emit Transfer(address(0), to, amount); // Emulate minting like ERC-20
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        require(to != address(0), "Invalid address");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "Invalid spender");
+
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "Allowance exceeded");
+        require(to != address(0), "Invalid recipient");
+
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+        return true;
     }
 }
+
+
